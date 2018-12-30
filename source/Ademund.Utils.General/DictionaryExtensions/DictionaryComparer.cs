169fa6c6df92acfcab049e7ce047e608fd1c1d40
netclectic <adrian.cockburn@netclectic.com>
2018-12-30 19:54:41 +0000

    remove references to script api visible attribute

--- source/Ademund.Utils.General/DictionaryExtensions/DictionaryComparer.cs
+++ source/Ademund.Utils.General/DictionaryExtensions/DictionaryComparer.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.Serialization;
 
 namespace Ademund.Utils.DictionaryExtensions
 {
@@ -48,81 +46,4 @@ namespace Ademund.Utils.DictionaryExtensions
 			return dictionary.All(kvp => kvp.Value == 0);
 		}
 	}
-
-	[Serializable]
-	public class DictionaryWithDefault<TKey, TValue> : Dictionary<TKey, TValue>
-	{
-        public TValue DefaultValue { get; set; }
-        public DictionaryWithDefault() : base() { }
-		public DictionaryWithDefault(IDictionary<TKey, TValue> dictionary) : base(dictionary) { }
-
-		public DictionaryWithDefault(TValue defaultValue): base()
-		{
-			DefaultValue = defaultValue;
-		}
-
-		public DictionaryWithDefault(SerializationInfo info, StreamingContext context): base(info, context) { }
-
-		public new TValue this[TKey key]
-		{
-			get
-			{
-				TValue t = DefaultValue;
-				base.TryGetValue(key, out t);
-				return t;
-			}
-			set
-			{
-				if (EqualityComparer<TValue>.Default.Equals(value, default(TValue)) && base.ContainsKey(key))
-					base.Remove(key);
-				else
-					base[key] = value;
-			}
-		}
-
-        [ScriptAPIVisible(true)]
-        public new int Count => base.Count;
-
-        [ScriptAPIVisible(true)]
-        public new KeyCollection Keys => base.Keys;
-
-        [ScriptAPIVisible(true)]
-        public new ValueCollection Values => base.Values;
-
-        [ScriptAPIVisible(true)]
-        public new void Add(TKey key,TValue value)
-        {
-            base.Add(key, value);
-        }
-
-        [ScriptAPIVisible(true)]
-        public new void Clear()
-        {
-            base.Clear();
-        }
-
-        [ScriptAPIVisible(true)]
-        public new bool ContainsKey(TKey key)
-        {
-            return base.ContainsKey(key);
-        }
-
-        [ScriptAPIVisible(true)]
-        public new bool ContainsValue(TValue value)
-        {
-            return base.ContainsValue(value);
-        }
-
-        [ScriptAPIVisible(true)]
-        public new bool Remove(TKey key)
-        {
-            return base.Remove(key);
-        }
-
-        [ScriptAPIVisible(true)]
-        public new bool TryGetValue(TKey key,out TValue value)
-        {
-            return base.TryGetValue(key, out value);
-        }
-    }
 }
---
+++ source/Ademund.Utils.General/DictionaryExtensions/DictionaryWithDefault.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Ademund.Utils.DictionaryExtensions
+{
+    [Serializable]
+	public class DictionaryWithDefault<TKey, TValue> : Dictionary<TKey, TValue>
+	{
+        public TValue DefaultValue { get; set; }
+        public DictionaryWithDefault() : base() { }
+		public DictionaryWithDefault(IDictionary<TKey, TValue> dictionary) : base(dictionary) { }
+
+		public DictionaryWithDefault(TValue defaultValue): base()
+		{
+			DefaultValue = defaultValue;
+		}
+
+		public DictionaryWithDefault(SerializationInfo info, StreamingContext context): base(info, context) { }
+
+		public new TValue this[TKey key]
+		{
+			get
+			{
+				TValue t = DefaultValue;
+				base.TryGetValue(key, out t);
+				return t;
+			}
+			set
+			{
+				if (EqualityComparer<TValue>.Default.Equals(value, default(TValue)) && base.ContainsKey(key))
+					base.Remove(key);
+				else
+					base[key] = value;
+			}
+		}
+
+        public new int Count => base.Count;
+        public new KeyCollection Keys => base.Keys;
+        public new ValueCollection Values => base.Values;
+
+        public new void Add(TKey key,TValue value)
+        {
+            base.Add(key, value);
+        }
+
+        public new void Clear()
+        {
+            base.Clear();
+        }
+
+        public new bool ContainsKey(TKey key)
+        {
+            return base.ContainsKey(key);
+        }
+
+        public new bool ContainsValue(TValue value)
+        {
+            return base.ContainsValue(value);
+        }
+
+        public new bool Remove(TKey key)
+        {
+            return base.Remove(key);
+        }
+
+        public new bool TryGetValue(TKey key,out TValue value)
+        {
+            return base.TryGetValue(key, out value);
+        }
+    }
+}
